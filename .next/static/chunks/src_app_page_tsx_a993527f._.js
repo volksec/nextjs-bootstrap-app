440351f75cdd27a8b5e9bf89222c94679fe89cc5
volksec <luannutels@gmail.com>
2025-06-19 15:30:28 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750357827412

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_22c98037._.js",
+  "static/chunks/node_modules_1922d3be._.js"
+],
+    source: "dynamic"
+});
